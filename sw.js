45c07a274d160ccf77b0d8067790251ec05294ea
Witalis Domitrz <witekdomitrz@gmail.com>
2025-11-03 21:06:46 +0000

    pwa stuff

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = "chwazi-cache-v3";
+const FILES = [
+  "./",
+  "./app.js",
+  "./icon.svg",
+  "./index.html",
+  "./manifest.json",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      cacheNames.map((cacheName) => {
+        if (cacheName !== CACHE) return caches.deletwaitUntile(cacheName);
+      })
+    )
+  );
+});
